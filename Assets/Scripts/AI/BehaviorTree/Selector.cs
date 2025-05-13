cec065c8b3fe129143bcad3e8a1f43996aedcd6f
Josh Acosta <jodacost@ucsc.edu>
2025-05-13 01:55:55 -0700

    Selector complete

--- Assets/Scripts/AI/BehaviorTree/Selector.cs
+++ Assets/Scripts/AI/BehaviorTree/Selector.cs
@@ -4,6 +4,17 @@ public class Selector : InteriorNode
 {
     public override Result Run()
     {
+        while (current_child < children.Length)
+        {
+            var status = children[current_child].Run();
+            if (status == Result.SUCCESS)
+                return Result.SUCCESS;
+            if (status == Result.IN_PROGRESS)
+                return Result.IN_PROGRESS;
+
+            current_child++;
+        }
+
         return Result.FAILURE;
     }
 
@@ -14,6 +25,14 @@ public class Selector : InteriorNode
     public override BehaviorTree Copy()
     {
         return new Selector(CopyChildren());
+    }
+
+    public override IEnumerable<BehaviorTree> AllNodes()
+    {
+        yield return this;
+        foreach (var child in children)
+            foreach (var node in child.AllNodes())
+                yield return node;
     }
 
 }
